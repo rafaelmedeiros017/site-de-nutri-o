de0ceb19304fc434e43502c111fa64354e66dd48
rafaelmedeiros017 <169009926+rafaelmedeiros017@users.noreply.github.com>
2024-06-13 00:26:20 -0300

    slide.js

--- site de nutrição/slide.js
+++ site de nutrição/slide.js
@@ -2,9 +2,20 @@ $('.slider-principal').slick({
 dots: true,
 infinite:true,
 spped:300,
-slidestoshow:1,
+slidestoshow:3,
 adaptiveheight:true,
 autoplayspeed:2000
 
 });
-
+let cout=1;
+document.getElementById("slider-principal").checked=true;
+setInterval(function(){
+    nextimage();
+},2000)
+function nextimage(){
+    cout++;
+    if(cout>3){
+        cout=1;
+    }
+    document.getElementById("slider-principal"+cout).checked=true;
+}
